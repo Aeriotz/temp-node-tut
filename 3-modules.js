fd4c4d76c005aa37ccf439690ea8266619587ca4
Aeriotz <nakarin_te@hotmail.com>
2023-03-14 16:15:12 +0700

    first commit

---
+++ 3-modules.js
@@ -0,0 +1,9 @@
+// ComminJS, every file is module (by default)
+// Modules - Encapsulated Code
+const names = require("./4-name");
+const sayHi = require("./5-utils");
+const data = require("./6-alternative-flavor");
+require("./7-mind-grenade");
+sayHi("susan");
+sayHi(names.john);
+sayHi(names.peter);
